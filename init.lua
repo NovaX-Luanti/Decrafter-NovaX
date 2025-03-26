ac6e5ab0ce5553c5ff56b9c3f53829b807ef690c
NovaX <lupogovirus@gmail.com>
2025-03-26 13:18:36 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,18 +4,17 @@ minetest.register_node("decrafter:table", {
     groups = {choppy = 2, oddly_breakable_by_hand = 2},
 
     on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
-		local inv = meta:get_inventory()
-		inv:set_size("input", 1)
-		inv:set_size("output", 9)
-		meta:set_string("formspec", "size[10.5,8.5]" ..
-			"label[1,1;Oggetto da decraftare]" ..
-			"list[current_name;input;1,2;1,1;]" ..
-			"label[5,0.5;Materiali ottenuti]" ..
-			"list[current_name;output;5,1;3,3;]" ..
-			"list[current_player;main;1,4.5;8,4;]")
-	end,
-
+        local meta = minetest.get_meta(pos)
+        local inv = meta:get_inventory()
+        inv:set_size("input", 1)
+        inv:set_size("output", 9)
+        meta:set_string("formspec", "size[10.5,8.5]" ..
+            "label[1,1;Item to decraft]" ..
+            "list[current_name;input;1,2;1,1;]" ..
+            "label[5,0.5;Recovered materials]" ..
+            "list[current_name;output;5,1;3,3;]" ..
+            "list[current_player;main;1,4.5;8,4;]")
+    end,
 
     allow_metadata_inventory_put = function(pos, listname, index, stack, player)
         if listname == "input" then
@@ -41,7 +40,7 @@ minetest.register_node("decrafter:table", {
                 inv:set_list("output", output)
             end
         else
-            inv:set_list("output", {}) -- svuota se non c'è nulla
+            inv:set_list("output", {})
         end
     end,
 
@@ -51,7 +50,6 @@ minetest.register_node("decrafter:table", {
             local inv = meta:get_inventory()
             local input_stack = inv:get_stack("input", 1)
             if not input_stack:is_empty() then
-                -- Rimuove l’oggetto solo quando si inizia a prendere dalla griglia
                 inv:set_stack("input", 1, nil)
             end
         end
@@ -64,13 +62,15 @@ minetest.register_node("decrafter:table", {
             local inv = meta:get_inventory()
 
             local all_items = inv:get_list("output")
-            local leftover = player:get_inventory():add_item("main", stack)
+            player:get_inventory():add_item("main", stack)
+
             for i, item in ipairs(all_items) do
                 if i ~= index then
                     player:get_inventory():add_item("main", item)
                 end
             end
-            inv:set_list("output", {}) -- pulisce la griglia dopo aver preso
+
+            inv:set_list("output", {})
         end
     end,
 })
